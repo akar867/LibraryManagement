96c90202192f9d126576fde810732b306bfde4eb
akar867 <kararnab99@gmail.com>
2023-09-26 21:11:37 +0530

    LibraryManagement

---
+++ library/src/main/java/com/application/courselibrary/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.application.library.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+    @GetMapping("/")
+    public String listHome(){
+        return "index";
+    }
+}
